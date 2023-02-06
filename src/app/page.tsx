a64d4d74227abd349de615845cac6e2e175a56f3
Kevin Willian <kevinwillian084@gmail.com>
2023-02-05 22:51:33 -0300

    feat: Fix chakra ui bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import { Flex } from '@chakra-ui/react';
+
 export default function Home() {
-  return <div>Welcome</div>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      Welcome
+      <Flex as="form" w="100%" maxW="360px">
+        Now chakra-ui is working
+      </Flex>
+    </Flex>
+  );
 }
