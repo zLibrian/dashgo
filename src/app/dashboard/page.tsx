862519a95e04657e1c0f0024ece6363883ead542
Kevin Willian <kevinwillian084@gmail.com>
2023-02-11 22:19:15 -0300

    feat: add sidebar to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import { Header } from '@/components/Header';
+import { Sidebar } from '@/components/Sidebar';
+import { Flex } from '@chakra-ui/react';
 
 export default function Dashboard() {
   return (
     <div>
-      <Header />
+      <Flex direction="column" h="100vh" maxW={1480} mx="auto">
+        <Header />
+        <Flex w="full" bg="red" my="6" px="6">
+          <Sidebar />
+        </Flex>
+      </Flex>
     </div>
   );
 }
