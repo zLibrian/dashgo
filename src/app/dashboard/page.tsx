88aa587e2fce4cabd30a4c572dda9918088539bd
Kevin Willian <kevinwillian084@gmail.com>
2023-02-13 13:44:11 -0300

    fix: fix a bug where the background color of dashboard was red

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,15 +2,16 @@
 
 import { Header } from '@/components/Header';
 import { Sidebar } from '@/components/Sidebar';
-import { Flex } from '@chakra-ui/react';
+import { Flex, SimpleGrid } from '@chakra-ui/react';
 
 export default function Dashboard() {
   return (
     <div>
       <Flex direction="column" h="100vh" maxW={1480} mx="auto">
         <Header />
-        <Flex w="full" bg="red" my="6" px="6">
+        <Flex w="full" my="6" px="6">
           <Sidebar />
+          <SimpleGrid></SimpleGrid>
         </Flex>
       </Flex>
     </div>
