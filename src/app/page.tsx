1f83c1fa34c717292cee902f4b9286cae587c69b
Kevin Willian <kevinwillian084@gmail.com>
2023-02-18 19:40:22 -0300

    refactor: add react hook form in the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 'use client';
 
-import { Flex, Button, Stack, FormControl } from '@chakra-ui/react';
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 import { Form } from '@/components/Form';
 import { Input } from '@/components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit } = useForm<SignInFormData>();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = (data, e) => {
+    e?.preventDefault();
+    console.log(data);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
-      <Form>
+      <Form onSubmit={handleSubmit(handleSignIn)}>
         <Stack spacing="4">
-          <Input label="Email:" name="email" type="email" />
-          <Input label="Password:" name="password" type="password" />
+          <Input label="Email:" type="email" {...register('email')} />
+          <Input label="Password:" type="password" {...register('password')} />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">
