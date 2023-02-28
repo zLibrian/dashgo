889e755aa0783e6bac3b6e1e0c8e7a0d0b564536
Kevin Willian <kevinwillian084@gmail.com>
2023-02-27 22:09:58 -0300

    feat: add responsiveness to the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,41 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
 
+import { useSidebarDrawer } from '@/context/SidebarDrawerContext';
+
+import { Logo } from './Logo';
 import { NotificationNav } from './NotificationNav';
 import { Profile } from './Profile';
 import { SearchBar } from './SearchBar';
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  const { onOpen } = useSidebarDrawer();
+
   return (
     <Flex as="header" w="100%" h="20" mt="4" px="6" align="center">
-      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
-        Dashgo
-        <Text as="span" color="pink.500" ml="1">
-          .
-        </Text>
-      </Text>
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<RiMenuLine />}
+          onClick={onOpen}
+          variant="unstyled"
+          fontSize="24"
+          textAlign="center"
+          mr="2"
+        />
+      )}
+      <Logo />
 
-      <SearchBar />
+      {isWideVersion && <SearchBar />}
 
       <Flex ml="auto" align="center">
         <NotificationNav />
-        <Profile />
+        <Profile showProfile={isWideVersion} />
       </Flex>
     </Flex>
   );
